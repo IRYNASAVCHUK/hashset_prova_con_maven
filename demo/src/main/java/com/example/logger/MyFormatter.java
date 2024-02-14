cc160e52b9f0a128409bc9e867e3a6f0b5be0ccc
IRYNASAVCHUK <irinka25041990@gmail.com>
2024-02-14 14:10:50 +0100

    add primitive type

--- demo/src/main/java/com/example/logger/MyFormatter.java
+++ demo/src/main/java/com/example/logger/MyFormatter.java
@@ -17,35 +17,38 @@ public class MyFormatter extends Formatter {
         ObjectNode jsonNode = objectMapper.createObjectNode().put("event", event);
 
         Object[] params = record.getParameters();
-        MyRecord<?> myRecord = (MyRecord<?>) params[0];
-        
+        // TODO: non sono sicura che si fa cosi
+        // FIXME: target
         Object target = null;
         if (params != null && params.length > 0) {
             if (params[0] instanceof MyRecord<?>) {
+                MyRecord<?> myRecord = (MyRecord<?>) params[0];
                 target = myRecord.result();
-            } else
+            } else {
                 target = params[0];
+            }
         }
-
-        if (target == null) 
+        if (target == null)
             jsonNode.putNull("target");
-        else if (target instanceof Class<?>) 
-                jsonNode.put("target", ((Class<?>) target).getName());
-        else 
-                jsonNode.put("target", System.identityHashCode(target));
-        
+        else if (target instanceof Class<?>)
+            jsonNode.put("target", ((Class<?>) target).getName());
+        else
+            jsonNode.put("target", System.identityHashCode(target));
+
         if (params != null && params.length > 0) {
             if (params[0] instanceof MyRecord) {
+                MyRecord<?> myRecord = (MyRecord<?>) params[0];
                 // Controllo se params è vuoto
                 Object[] args = myRecord.params();
+                /*
+                 * addPOJO() è un metodo della libreria Jackson ObjectMapper che consente di
+                 * aggiungere un oggetto Java POJO (Plain Old Java Object) come valore a un nodo
+                 * di tipo ArrayNode o ObjectNode durante la serializzazione JSON. Questo metodo
+                 * si occupa automaticamente di convertire l'oggetto Java in un formato JSON
+                 * corrispondente.
+                 */
                 if (args != null && args.length > 0) {
                     ArrayNode argsNode = objectMapper.createArrayNode();
-/*
-    addPOJO() è un metodo della libreria Jackson ObjectMapper che consente di aggiungere 
-    un oggetto Java POJO (Plain Old Java Object) come valore a un nodo di tipo ArrayNode 
-    o ObjectNode durante la serializzazione JSON. Questo metodo si occupa automaticamente 
-    di convertire l'oggetto Java in un formato JSON corrispondente.
-*/                   
                     for (Object arg : args)
                         argsNode.addPOJO(arg);
                     jsonNode.set("args", argsNode);
@@ -54,10 +57,50 @@ public class MyFormatter extends Formatter {
                 Class<?> returnType = myRecord.returnType();
                 if (!returnType.equals(void.class)) {
                     Object returnValue = myRecord.result();
-                    if (returnValue != null)
-                        jsonNode.putPOJO("returnValue", returnValue);
+                    if (returnValue != null) {
+                        // Verifica se il risultato è un tipo primitivo
+                        // The primitive Java types: boolean, byte, char, short, int, long, float,
+                        // double
+                        if (returnType.isPrimitive()) {
+                            switch (returnType.getTypeName()) {
+                                case "boolean":
+                                    boolean boolValue = (boolean) returnValue;
+                                    jsonNode.put("result", boolValue);
+                                    break;
+                                case "byte":
+                                    byte byteValue = (byte) returnValue;
+                                    jsonNode.put("result", byteValue);
+                                    break;
+                                case "char":
+                                    char charValue = (char) returnValue;
+                                    jsonNode.put("result", charValue);
+                                    break;
+                                case "short":
+                                    short shortValue = (short) returnValue;
+                                    jsonNode.put("result", shortValue);
+                                    break;
+                                case "int":
+                                    int intValue = (int) returnValue;
+                                    jsonNode.put("result", intValue);
+                                    break;
+                                case "long":
+                                    long longValue = (long) returnValue;
+                                    jsonNode.put("result", longValue);
+                                    break;
+                                case "float":
+                                    float floatValue = (float) returnValue;
+                                    jsonNode.put("result", floatValue);
+                                    break;
+                                case "double":
+                                    double doubleValue = (double) returnValue;
+                                    jsonNode.put("result", doubleValue);
+                                    break;
+                            }
+                        }
+                    }
+
                     else
-                        jsonNode.putNull("returnValue");
+                        jsonNode.putNull("result");
                 }
             } else {
                 // Gestisco il caso in cui params non è un'istanza di MyRecord, ma un Object[]
