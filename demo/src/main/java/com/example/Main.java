89a4f3dc863fc26da752ef4c2329f1030c5ab647
IRYNASAVCHUK <irinka25041990@gmail.com>
2024-04-08 16:10:00 +0200

    push

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,32 +1,34 @@
 package com.example;
 
 import com.example.hashset.Counter;
-//import com.example.hashset.Customer;
-//import com.example.hashset.MyHashSet;
+import com.example.hashset.Customer;
+import com.example.hashset.MyHashSet;
 
 public class Main {
     public static void main(String[] args) {
-        // final MyHashSet example = new MyHashSet();
-        // Customer person1 = new Customer(0, "Mario");
-        // Customer person2 = new Customer(1, "Marco");
-        // Customer person3 = new Customer(2, "Matteo");
-        // example.addCustomer(person1);
-        // example.addCustomer(person2);
-        // example.addCustomer(person3);
-        // example.addCustomer(person3);
-        // example.removeCustomer(person1);
-        // example.removeCustomer(person1);
-        // example.containsCustomer(person3);
-        // example.getSize();
+        final MyHashSet example = new MyHashSet();
+        Customer person1 = new Customer(0, "Mario");
+        Customer person2 = new Customer(1, "Marco");
+        Customer person3 = new Customer(2, "Matteo");
+        example.addCustomer(person1);
+        example.addCustomer(person2);
+        example.addCustomer(person3);
+        example.addCustomer(person3);
+        example.removeCustomer(person1);
+        example.removeCustomer(person1);
+        example.containsCustomer(person3);
+        example.getSize();
 
-        // // metodo statico
-        // MyHashSet.sum(2, 2);
-        // example.clearSet();
+        // metodo statico
+        MyHashSet.sum(2, 2);
+        example.clearSet();
 
-        // // Creazione di un oggetto Customer
-        // Customer customer = new Customer(1, "John Doe");
-        // customer.getId();
-        // customer.getName();
+        // Creazione di un oggetto Customer
+        Customer customer = new Customer(1, "John Doe");
+        customer.getId();
+        customer.getName();
+        
+        // new 
         Counter c1, c2;
         c1 = new Counter();
         c1.inc(); 
