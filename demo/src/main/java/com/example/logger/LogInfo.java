0a93aa281855eb1ad0a346d672ae8df0e6bbfef1
IRYNASAVCHUK <irinka25041990@gmail.com>
2024-04-17 18:34:28 +0200

    first commit

---
+++ demo/src/main/java/com/example/logger/LogInfo.java
@@ -0,0 +1,41 @@
+package com.example.logger;
+
+import java.util.logging.LogRecord;
+import com.example.record.*;
+
+public class LogInfo {
+    private String event;
+    private String target;
+    private Object[] args;
+    private Object result;
+    private String name;
+
+    public LogInfo(LogRecord record) {
+        this.event = record.getMessage();
+
+        Object[] params = record.getParameters();
+        Object targetsObject = null;
+
+        if (params == null || params.length == 0)
+            throw new IllegalArgumentException("No parameters found in the record.");
+
+        if (params[0] instanceof MyRecord) {
+            MyRecord myRecord = (MyRecord) params[0];
+            if (myRecord instanceof MyRecordExiting) {
+                MyRecordExiting<?> exitingRecord = (MyRecordExiting<?>) myRecord;
+                targetsObject = exitingRecord.thisObject();
+            } else if (myRecord instanceof MyRecordEntering) {
+                MyRecordEntering enteringRecord = (MyRecordEntering) myRecord;
+                targetsObject = enteringRecord.thisObject();
+            }
+            this.target = (targetsObject == null) ? record.getSourceClassName()
+                    : Integer.toString(System.identityHashCode(targetsObject));
+            this.args = myRecord.params();
+            if (myRecord instanceof MyRecordExiting){
+                MyRecordExiting<?> exitingRecord = (MyRecordExiting<?>) myRecord;
+                this.result = exitingRecord.result() == null ? null : exitingRecord.result();
+            }
+            this.name = record.getSourceClassName() + "." + record.getSourceMethodName();
+        }
+    }
+}
--- demo/src/main/java/com/example/logger/MyFormatter.java
+++ demo/src/main/java/com/example/logger/MyFormatter.java
@@ -1,125 +1,20 @@
 package com.example.logger;
 
-import com.example.record.*;
+import com.google.gson.Gson;
+// import com.google.gson.GsonBuilder; prettyString
 
 import java.util.logging.*;
 // provare Gson
-// string bulder o stingFormat
+
 public class MyFormatter extends Formatter {
+    private Gson gson = new Gson();//new GsonBuilder().setPrettyPrinting().create(); // <--semplice
+
 
     @Override
     public String format(LogRecord record) {
-        String event = record.getMessage();
-
-        String resultString = "{\n\t\t\"event\": \"" + event + "\",";//record.getMessage()  //file di config // fare meno ripetitivo
-
-        Object[] params = record.getParameters();
-
-        Object target = null;
-
-        if (params == null || params.length == 0)
-            throw new IllegalArgumentException("No parameters found in the record.");
-
-        if (params[0] instanceof MyRecord) {
-            MyRecord myRecord = (MyRecord) params[0];
-            if (myRecord instanceof MyRecordExiting) {
-                MyRecordExiting<?> exitingRecord = (MyRecordExiting<?>) myRecord;
-                target = exitingRecord.thisObject();
-            } else if (myRecord instanceof MyRecordEntering) {
-                MyRecordEntering enteringRecord = (MyRecordEntering) myRecord;
-                target = enteringRecord.thisObject();
-            }
-            // chiamate di hashCode() inaspetatti : addCustomer, removeCustomer,
-            // containsCustomer,
-            resultString += "\n\t\t\"target\": \"";
-            resultString += (target == null) ? record.getSourceClassName() : System.identityHashCode(target);
-            resultString += "\",";
-            Object[] args = myRecord.params();
-            if (args != null && args.length > 0) {
-                resultString += "\n\t\t\"args\": [";
-                for (Object arg : args) {
-                    if (isPrimitiveOrWrapper(arg)) {
-                        resultString += arg+ ",";
-                    } else if (arg instanceof String) {
-                        resultString += "\"" + arg + "\",";
-                    } else
-                        resultString += "\"" + System.identityHashCode(arg) + "\",";
-                }
-                resultString = resultString.substring(0, resultString.length() - 1); // Rimuove l'ultima virgola
-                resultString += "],";
-            }
-            if (myRecord instanceof MyRecordExiting<?>) {
-                Class<?> returnType = ((MyRecordExiting<?>) myRecord).returnType();
-                if (!returnType.equals(void.class)) {
-                    resultString += "\n\t\t\"result\": [";
-                    Object returnValue = ((MyRecordExiting<?>) myRecord).result();
-                    if (returnValue != null) {
-
-                        if (returnType.isPrimitive()) {
-                            resultString += primitiveReturnValue(returnType, returnValue);
-                        } else if (returnValue instanceof String) {
-                            resultString += "\"" + returnValue + "\"";
-                        } else if (isWrapper(returnValue)) {
-                            resultString += returnValue;
-                        } else {
-                            resultString += "\"" + System.identityHashCode(returnValue) + "\"";
-                        }
-                    } else
-                        resultString += "\"" + null + "\"";
-                    resultString += "],";
-                }
-            }
-        }
-        resultString += "\n\t\t\"name\": \"" + record.getSourceClassName() + "." + record.getSourceMethodName()
-                + "\"\n\t}";
-        return resultString;
+        LogInfo logInfo = new LogInfo(record);
+        return gson.toJson(logInfo);
     }
 
-    private String primitiveReturnValue(Class<?> returnType,
-            Object returnValue) {
-        String resultString = "";
-        switch (returnType.getTypeName()) {
-            case "boolean":
-                resultString += (boolean) returnValue;
-                break;
-            case "byte":
-                resultString += (byte) returnValue;
-                break;
-            case "char":
-                resultString += (char) returnValue;
-                break;
-            case "short":
-                resultString += (short) returnValue;
-                break;
-            case "int":
-                resultString += (int) returnValue;
-                break;
-            case "long":
-                resultString += (long) returnValue;
-                break;
-            case "float":
-                resultString += (float) returnValue;
-                break;
-            case "double":
-                resultString += (double) returnValue;
-                break;
-        }
-        return resultString;
-    }
-
-    private boolean isPrimitiveOrWrapper(Object obj) {
-        return obj.getClass().isPrimitive() || isWrapper(obj);
-    }
-
-    private boolean isWrapper(Object obj) {
-        return obj instanceof Integer ||
-                obj instanceof Double ||
-                obj instanceof Float ||
-                obj instanceof Character ||
-                obj instanceof Boolean ||
-                obj instanceof Short ||
-                obj instanceof Byte ||
-                obj instanceof Long;
-    }
-
+   
 }
