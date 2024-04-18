5a12c25bd6bd95acbb932afcf31f42719a407267
IRYNASAVCHUK <irinka25041990@gmail.com>
2024-04-18 09:56:08 +0200

    ConfigLoader change

--- demo/src/main/java/com/example/logger/ConfigLoader.java
+++ demo/src/main/java/com/example/logger/ConfigLoader.java
@@ -1,24 +1,23 @@
 package com.example.logger;
 
 import com.example.utils.Constants;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class ConfigLoader {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-    private static JsonNode configNode;
+    private static JsonObject configObject;
 
     static {
-        try {
-            configNode = objectMapper.readTree(new File(Constants.CONFIG_FILE));
+        try (FileReader reader = new FileReader(Constants.CONFIG_FILE)) {
+            configObject = JsonParser.parseReader(reader).getAsJsonObject();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static String getConfigValue(String key) {
-        return configNode.get(key).asText();
+        return configObject.get(key).getAsString();
     }
 }
