f1106a4f84da40c8bf0f078df934283a4fffd96a
davideancona <davide.ancona@unige.it>
2024-04-23 19:24:29 +0200

    Test.java, CAdapter1.java, CAdapter2.java

--- demo/src/main/java/com/example/Test.java
+++ demo/src/main/java/com/example/Test.java
@@ -1,71 +1,22 @@
 package com.example;
 
-import java.io.IOException;
-
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-
-class C {
-	C f1;
-	C f2;
-
-	C(C f1, C f2) {
-		this.f1 = f1;
-		this.f2 = f2;
-	}
-
-}
-
-class CAdapter extends TypeAdapter<C> {
-
-	int level;
-	Gson gson;
-
-	CAdapter(int level) {
-		this.level = level;
-	}
-
-	@Override
-	public void write(JsonWriter out, C value) throws IOException {
-//		if (value == null) {
-//			out.jsonValue(null);
-//			return;
-//		}
-		out.beginObject();
-		out.name("@");
-		out.value(System.identityHashCode(value));
-		out.name("class");
-		out.value(value.getClass().getCanonicalName());
-		if (level > 0) {
-			level--;
-			out.name("f1");
-			out.jsonValue(gson.toJson(value.f1));
-			out.name("f2");
-			out.jsonValue(gson.toJson(value.f2));
-			level++;
-		}
-		out.endObject();
-	}
-
-	@Override
-	public C read(JsonReader in) throws IOException {
-		throw new UnsupportedOperationException();
-	}
-
-}
 
 public class Test {
 
 	public static void main(String[] args) {
+		final var level = 4;
 		var c = new C(new C(new C(null, null), new C(null, null)), new C(new C(null, null), new C(null, null)));
-		var builder = new GsonBuilder();
-		var adapter = new CAdapter(4);
-		builder.registerTypeAdapter(C.class, adapter).setPrettyPrinting();
-		adapter.gson = builder.create();
-		System.out.println(adapter.gson.toJson(c));
+		var builder1 = new GsonBuilder();
+		var adapter1 = new CAdapter1(level);
+		builder1.registerTypeAdapter(C.class, adapter1).setPrettyPrinting();
+		var gson = builder1.create();
+		System.out.println(gson.toJson(c));
+		var builder2 = new GsonBuilder();
+		var adapter2 = new CAdapter2(level);
+		builder2.registerTypeAdapter(C.class, adapter2).setPrettyPrinting();
+		adapter2.setGson(builder2);
+		System.out.println(adapter2.getGson().toJson(c));
 	}
 
 }
