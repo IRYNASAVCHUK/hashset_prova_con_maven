b64fac70c09d2e5d9854707c40cc3b3420bb4e51
IRYNASAVCHUK <irinka25041990@gmail.com>
2024-05-23 10:28:45 +0200

    upgrade record Levels

--- demo/src/main/java/com/example/project/logic/log_record/record/Levels.java
+++ demo/src/main/java/com/example/project/logic/log_record/record/Levels.java
@@ -1,18 +1,3 @@
 package com.example.project.logic.log_record.record;
 
-public record Levels(int target, int args, int result) {
-    public Levels decreaseTarget() {
-        return new Levels(target - 1, args, result);
-    }
-
-    public Levels decreaseArgs() {
-        return new Levels(target, args - 1, result);
-    }
-
-    public Levels decreaseResult() {
-        return new Levels(target, args, result - 1);
-    }
-    public Levels decreaseAll(){
-        return new Levels(target-1, args-1, result - 1);
-    }
-}
+public record Levels(int target, int args, int result) {}
