c9f45349b0a86aa57b20f6d70d1c94ba9b8b16ae
IRYNASAVCHUK <irinka25041990@gmail.com>
2024-07-04 11:18:17 +0200

    update

---
+++ demo/src/main/java/WebSocketMain.java
@@ -0,0 +1,29 @@
+
+
+import java.util.Scanner;
+
+import org.glassfish.tyrus.server.Server;
+
+import core.connection.WebSocketServer;
+import core.utils.Constants;
+
+public class WebSocketMain {
+    public static void main(String[] args) {
+        Server server = new Server(Constants.URL, Constants.PORT, "/ws", WebSocketServer.class);
+        try {
+            server.start();
+            System.out.println("[SERVER]: Server started at ws://"
+                    + Constants.URL + ":" + Constants.PORT + "/ws/endpoint");
+            System.out.println("[SERVER]: Press 'q' to stop the server...");
+            try (Scanner scanner = new Scanner(System.in)) {
+                while (!scanner.nextLine().equalsIgnoreCase("q")) {
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            server.stop();
+            System.out.println("[SERVER]: Server stopped.");
+        }
+    }
+}
--- demo/src/main/java/core/connection/WebSocketServer.java
+++ demo/src/main/java/core/connection/WebSocketServer.java
@@ -1,36 +1,11 @@
 package core.connection;
 
 import java.io.IOException;
-import java.util.Scanner;
-
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 
-import org.glassfish.tyrus.server.Server;
-
-import core.utils.Constants;
-
 @ServerEndpoint("/endpoint")
-public class WebSocketServer {
-    public static void main(String[] args) {
-        Server server = new Server(Constants.URL, Constants.PORT, "/ws", WebSocketServer.class);
-        try {
-            server.start();
-            System.out.println("[SERVER]: Server started at ws://"
-                    + Constants.URL + ":" + Constants.PORT + "/ws/endpoint");
-            System.out.println("[SERVER]: Press 'q' to stop the server...");
-            try (Scanner scanner = new Scanner(System.in)) {
-                while (!scanner.nextLine().equalsIgnoreCase("q")) {
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            server.stop();
-            System.out.println("[SERVER]: Server stopped.");
-        }
-    }
-    
+public class WebSocketServer {    
     @OnOpen
     public void onOpen(Session session) {
         System.out.println("[SERVER]: Connection established with session ID: " + session.getId());
